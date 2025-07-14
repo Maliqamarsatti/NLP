03486bd6d9dd1025d14ef1cf8dc3cc6c904424d7
Maliqamarsatti <alixatte1235@gmail.com>
2025-07-14 20:42:43 +0100

    Add coffee disease detection code

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(
-    data="data.yaml",   # path to your data config
-    epochs=30,          # total number of epochs
-    imgsz=640,          # input image size
-    batch=16,           # batch size
-    name="coffee_model",# output folder runs/detect/coffee_model
-    save_period=5       # save checkpoint every 5 epochs
+    data="data.yaml",   
+    epochs=30,          
+    imgsz=640,          
+    batch=16,           
+    name="coffee_model",
+    save_period=5       
 )
